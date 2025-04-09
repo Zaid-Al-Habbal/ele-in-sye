42b8a3bf213320435e4329d7dca495994057bac2
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-04-09 17:32:50 +0300

    add CategorySerializer

---
+++ categories/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from categories.models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            'id',
+            'name',
+            'slug',
+            'created_at',
+            'updated_at',
+            'parent'
+        ]
+    
